8206850f8127da155607022ff25ac22337b6d827
NTB-60 <NTB-60@host.docker.internal>
2022-07-04 15:49:32 +0300

    if else 001

---
+++ JavaTutorial/src/IfElse.java
@@ -0,0 +1,17 @@
+
+public class IfElse {
+
+	public static void main(String[] args) {
+
+		int a = 50, b = 23;
+		
+		if (a == b) {
+			System.out.println("A and B equal ");
+		}else {
+			System.out.println("A and b NOT equal");
+		}
+		
+		
+	}
+
+}
