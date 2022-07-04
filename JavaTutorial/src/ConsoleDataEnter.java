486d0daeb626aafebe7c1a7075820558c024a2e4
NTB-60 <NTB-60@host.docker.internal>
2022-07-04 15:09:31 +0300

    JavaScanner

---
+++ JavaTutorial/src/ConsoleDataEnter.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ConsoleDataEnter {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int a;
+		int b;
+		
+		System.out.println("A Sayisini Giriniz: " );
+		a = input.nextInt();
+		System.out.println("B Sayisini Giriniz: " );
+		b = input.nextInt();
+		
+		System.out.println("A + B = "+ (a +b));
+		System.out.println("A - B = "+ (a - b));
+	}
+
+}
