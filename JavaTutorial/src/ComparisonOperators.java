e1277e01c56ab8eccccc15504a95878632460609
NTB-60 <NTB-60@host.docker.internal>
2022-07-04 15:44:13 +0300

    Comparison Operators

---
+++ JavaTutorial/src/ComparisonOperators.java
@@ -0,0 +1,13 @@
+
+public class ComparisonOperators {
+
+	public static void main(String[] args) {
+		int a = 10, b = 22;
+		boolean result = (a == b);
+		System.out.println(result);
+		
+		System.out.println(!result);
+
+	}
+
+}
