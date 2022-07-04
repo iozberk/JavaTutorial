24f63f4f9e008f6ca9e2f3a22761b7af1dba1764
NTB-60 <NTB-60@host.docker.internal>
2022-07-04 15:53:32 +0300

    ifelse 002

--- JavaTutorial/src/IfElse.java
+++ JavaTutorial/src/IfElse.java
@@ -3,14 +3,25 @@ public class IfElse {
 
 	public static void main(String[] args) {
 
-		int a = 50, b = 23;
-		
+		 
+		int a = 50, b = 23, c = 12;
+		/*		
 		if (a == b) {
 			System.out.println("A and B equal ");
 		}else {
 			System.out.println("A and b NOT equal");
 		}
+		 */
 		
+		if (a <= b) {			
+			System.out.println("A smaller or equal B ");
+		} 
+		else if(a >= b){
+			System.out.println("A bigger than B");
+		}
+		else {
+			System.out.println("A and B NOT equal ");
+		}
 		
 	}
 
