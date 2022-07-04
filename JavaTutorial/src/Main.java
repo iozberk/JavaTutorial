42c676747cd1752d994aad1c24bcf89e356c897f
NTB-60 <NTB-60@host.docker.internal>
2022-07-04 10:58:15 +0300

    commit next

--- JavaTutorial/src/Main.java
+++ JavaTutorial/src/Main.java
@@ -1,9 +1,11 @@
 
 public class Main {
-
+	int x = 5;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("TEst");
+		Main myObj1= new Main(); // Object 1
+		Main myObj2= new Main(); // Object 2
+		System.out.println(myObj1.x);
+		System.out.println(myObj2.x);
 	}
 
 }
