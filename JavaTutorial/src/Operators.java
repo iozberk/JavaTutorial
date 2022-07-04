fcf15e738bf0e1613b30c525df53801d4afb09f8
NTB-60 <NTB-60@host.docker.internal>
2022-07-04 14:56:29 +0300

    iislemler

---
+++ JavaTutorial/src/Operators.java
@@ -0,0 +1,19 @@
+
+public class Operators {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 5;
+		int toplam = a + b;
+		int cikarma = a - b;
+		int carpma = a * b;
+		int bolme = a / b;
+		
+		System.out.println("Toplama: " + toplam);
+		System.out.println("Cikarma: " + cikarma);
+		System.out.println("Carpma: " + carpma);
+		System.out.println("Bolme: " + bolme);
+		}
+
+}
