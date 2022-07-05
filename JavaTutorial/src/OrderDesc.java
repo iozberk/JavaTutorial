ac38bffce938cbac06609b8bce0b8ac6b9d4ad7c
NTB-60 <NTB-60@NTB-60>
2022-07-05 10:56:04 +0300

    Order 3 number desc error fixed

--- JavaTutorial/src/OrderDesc.java
+++ JavaTutorial/src/OrderDesc.java
@@ -1,16 +1,12 @@
 import java.util.Scanner;
-
 public class OrderDesc {
-
 	public static void main(String[] args) {
-
 		Scanner input = new Scanner(System.in);
 		int a, b, c;
 		System.out.println("Enter numbers: ");
 		a = input.nextInt();
 		b = input.nextInt();
 		c = input.nextInt();
-
 		if (a > b && a > c) {
 			if (b > c) {
 				System.out.println("a > b > c");
@@ -22,13 +18,14 @@ public class OrderDesc {
 				System.out.println("b > a > c");
 			} else {
 				System.out.println("b > c > a ");
+			}	
+		}	
+		else {
+			if (a > b) {
+				System.out.println("c > a > b");
 		}
 			else {
-				if (a > b) {
-					System.out.println("c > a > b");
-				}
-				else {
-					System.out.println("c > b > a");
+				System.out.println("c > b > a");
 				}
 			}
 	}
