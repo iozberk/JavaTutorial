947c646fbe5afee068aee5ffb9f0c9d6c435f66b
NTB-60 <NTB-60@NTB-60>
2022-07-05 10:28:35 +0300

    others

--- JavaTutorial/src/FlightTicketPriceCalculator.java
+++ JavaTutorial/src/FlightTicketPriceCalculator.java
@@ -12,33 +12,43 @@ public class FlightTicketPriceCalculator {
 		Scanner input = new Scanner(System.in);
 		int age, kilometer, flighttype;
 		double kilometerprice = 0.1;
-		System.out.println("Please enter your age: ");
-		age = input.nextInt();
 		System.out.println("Please enter flight kilometer: ");
 		kilometer = input.nextInt();
+		System.out.println("Please enter your age: ");
+		age = input.nextInt();
+		
 		System.out.println("Please enter flight type: (1- OneWay\n 2-RoundTrip)");
 		flighttype = input.nextInt();
+		
+		double price, agediscount, flighttypediscount;
+		
 		if (kilometer > 0 && age > 0 && (flighttype == 1 || flighttype == 2)) {
-			System.out.println("Correct information ");
+			price = kilometer * kilometerprice;
+			if (age < 12) {
+				agediscount = price * 0.5;
+				price -= agediscount;
+			}
+			else if (age >= 12 && age <= 24) {
+				agediscount = price * 0.1;
+				price -= agediscount;
+			}
+			else if (age >= 65) {
+				agediscount = price * 0.3;
+				price -= agediscount;
+			}
+			else {
+				agediscount = 0;
+			}
+			if (flighttype == 2) {
+				flighttypediscount = price * 0.2;
+				price -= (price * flighttypediscount) * 0.2;				
+			}
+			System.out.println("Ticket Price: " + "$"+ price);
 		}
 		else {
 			System.out.println("Incorrect information ");
 		}
 		
-		double price;
-		
-		if (age <= 12) {
-			price = ((kilometer * kilometerprice) * 0.5);
-			System.out.println("Your Ticket Price: " + price);
-		}
-		else if (age >= 13 && age <= 24) {
-			price = ((kilometer * kilometerprice) * 0.9);
-			System.out.println("Your Ticket Price: " + price);
-		}
-		else if (age >= 65) {
-			price = ((kilometer * kilometerprice) * 0.7);
-			System.out.println("Your Ticket Price: " + price);
-		}
 	}
 
 }
