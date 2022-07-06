ebd0fdbbf324d05561b98af3a3af2a3a5ab70d79
NTB-60 <NTB-60@host.docker.internal>
2022-07-06 13:45:48 +0300

    For Loops 002

--- JavaTutorial/src/ForLoops.java
+++ JavaTutorial/src/ForLoops.java
@@ -2,8 +2,11 @@
 public class ForLoops {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		for(int i = 1; i <= 5; i++) {
+			System.out.println(i);
+		}
 	}
 
 }
