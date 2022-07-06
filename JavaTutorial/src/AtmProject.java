e283ad957e72dc5547cb4bf78f7a264d1ab2947b
NTB-60 <NTB-60@host.docker.internal>
2022-07-06 16:44:40 +0300

    Basic ATM Project

---
+++ JavaTutorial/src/AtmProject.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class AtmProject {
+
+	public static void main(String[] args) {
+		int balance = 10000, input, amount = 0;
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Welcome to JavaBank !!!");
+		System.out.println("Your Current Balance: " + balance);
+		
+		while(balance > 0) {
+			System.out.println();
+			System.out.println("1 - Deposit ");
+			System.out.println("2 - Withdrawal ");
+			System.out.println("3 - Inquiry ");
+			System.out.println("4 - Exit ");
+			System.out.println("Select Transaction: ");
+			input = scan.nextInt();
+			
+			if(input == 1) {
+				System.out.println("Please enter amount: ");
+				amount = scan.nextInt();
+				balance += amount;
+				System.out.println("New Balance after deposit: " + balance);
+			}else if(input == 2) {
+				System.out.println("Please enter amount: ");
+				if(amount > balance) {
+					System.out.println("Insufficient Balance");
+				}else {
+				amount = scan.nextInt();
+				balance -= amount;
+				System.out.println("New Balance after Withdrawal: " + balance);
+			}
+			}else if(input == 3) {
+				System.out.println("Balance: " + balance);
+			}else {
+				break;
+			}
+			
+		}
+		System.out.println("Thanks!!!");		
+		
+	}
+
+}
