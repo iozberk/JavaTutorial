205076eb121f31dbadb085be5428006e3e035b49
NTB-60 <NTB-60@host.docker.internal>
2022-07-06 13:48:43 +0300

    For Loops 003

--- JavaTutorial/src/ForLoops.java
+++ JavaTutorial/src/ForLoops.java
@@ -7,6 +7,20 @@ public class ForLoops {
 		for(int i = 1; i <= 5; i++) {
 			System.out.println(i);
 		}
+		System.out.println("-------------------------");
+		for(int i = 0; i <= 10; i = i + 2) {
+			System.out.println(i);
+		}
+		System.out.println("-------------------------");
+		for(int i = 0; i <= 20; i = i + 3) {
+			System.out.println(i);
+		}
+		
+		System.out.println("-------------------------");
+		for(int i = 20; i >= 0; i = i - 1) {
+			System.out.println(i);
+		}
 	}
+	
 
 }
