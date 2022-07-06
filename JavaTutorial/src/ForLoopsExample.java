af2778be4ef36e7140a9a57d6eabac4ec8465bae
NTB-60 <NTB-60@host.docker.internal>
2022-07-06 15:43:04 +0300

    Prime Number Calculator

---
+++ JavaTutorial/src/ForLoopsExample.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ForLoopsExample {
+
+	public static void main(String[] args) {
+		// Prime Number Calculator
+		Scanner input = new Scanner(System.in);
+		int prime;
+		
+		boolean	primee = true;
+		do {
+			System.out.println("Enter positive number: ");
+			prime = input.nextInt();
+		}while(prime < 2);
+		
+		for(int i = 2; i < prime; i++) {
+			if(prime % i == 0) {
+				primee = false;
+				break;
+			}
+		}
+		if(primee == true) {
+			System.out.println("Entered number: " + prime + " is prime");
+		}else {
+			System.out.println("Entered number: " + prime + " NOT prime");
+		}
+	}
+
+}
