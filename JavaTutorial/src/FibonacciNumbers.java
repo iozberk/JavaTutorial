1fd954c04982bafc21d616078685a43b8205c3cf
NTB-60 <NTB-60@host.docker.internal>
2022-07-06 16:35:14 +0300

    Fibonacci Numbers 0, 1, 1, 2, 3, 5, 8, 13, 21 ...

---
+++ JavaTutorial/src/FibonacciNumbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class FibonacciNumbers {
+
+	public static void main(String[] args) {
+		// Fibonacci Numbers 0, 1, 1, 2, 3, 5, 8, 13, 21 ...
+		int num1 = 0, num2 = 1;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter number: ");
+		int fib = input.nextInt();
+		for(int i = 1; i <= fib; i++) {
+			
+			System.out.print(num1 + " , ");
+			int total;
+			total = num1 + num2;
+			num1 = num2;
+			num2 = total;
+			
+		}
+	}
+
+}
