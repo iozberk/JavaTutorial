8925110f73a19521e14d8c6d5e200eb8febe4c3d
NTB-60 <NTB-60@host.docker.internal>
2022-07-06 16:57:23 +0300

    greeting method

---
+++ JavaTutorial/src/Methods.java
@@ -0,0 +1,15 @@
+
+public class Methods {
+
+	public static void main(String[] args) {
+		// f(x) = 2x + 2
+		greeting("John Jo");
+	}
+	
+	static void greeting(String a) {
+		String name = a;
+		System.out.println("Hello " + name);
+	}
+
+
+}
