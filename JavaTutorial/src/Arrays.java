4d5324c5e70ea057cdc4e4908af3cb9ece58d52e
iozberk <iozberks@gmail.com>
2022-07-08 23:34:56 +0300

    Arrays 001

---
+++ JavaTutorial/src/Arrays.java
@@ -0,0 +1,17 @@
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		
+		int[] list = new int[4]; // int[] list = {0,1,2,3,4,5};
+		list[0] = 0;
+		list[1] = 1;
+		list[2] = 2;
+		list[3] = 3;
+		
+		for(int i = 0; i < list.length; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
