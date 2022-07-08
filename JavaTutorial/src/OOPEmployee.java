2444783ddfbd061e85e1039759bcbd6df86b3594
iozberk <iozberks@gmail.com>
2022-07-08 23:15:49 +0300

    OOP 005

---
+++ JavaTutorial/src/OOPEmployee.java
@@ -0,0 +1,37 @@
+
+public class OOPEmployee {
+	private String name;
+	private double salary;
+	private int workHours, hireYear;
+	
+	
+	OOPEmployee(String name, double salary, int workHours, int hireYear){
+		this.name = name;
+		this.salary = salary;
+		this.workHours = workHours;
+		this.hireYear = hireYear;
+	}
+	
+	public double tax() {
+		if(this.salary >= 2000) {
+			return 2000 * 0.03;
+		}		
+		return 0.0;
+	}
+	public double bonuses() {
+		return 50.0 * this.workHours;
+	}
+	
+	public double payRaise() {
+		int year = 2022 - this.hireYear;
+		if(year < 10) {
+			return salary * 1.05;
+		} else if(year >= 10 && year < 20) {
+			return salary * 1.10;
+		} else {
+			return salary * 1.15;
+		}
+	}
+	
+	
+}
