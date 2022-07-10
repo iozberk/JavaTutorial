ebe118cac2ce00e6417471770d1a2c0c262c7d77
iozberk <iozberks@gmail.com>
2022-07-10 15:06:59 +0300

    Palindrome Word 001

---
+++ JavaTutorial/src/PalindromeWord.java
@@ -0,0 +1,16 @@
+
+public class PalindromeWord {
+
+	public static void main(String[] args) {
+		// https://en.wikipedia.org/wiki/Palindrome EX: TENET, ANNA, HANNAH, KAYAK, REFER etc...
+		String str = "kayak", temp = "";
+		for(int i = str.length() - 1; i >= 0; i--) {
+			temp += str.charAt(i);
+			// System.out.print(str.charAt(i) + " ");
+		}
+		if(temp.equals(str)) {
+			System.out.println(str + " is " + "Palindrome Word");
+		}
+	}
+
+}
