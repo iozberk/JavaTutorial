e48c2dffc2ce1837a7cdfa882f23fc7199a044da
iozberk <iozberks@gmail.com>
2022-07-10 13:54:57 +0300

    Guess number Game 002

--- JavaTutorial/src/NumberGuessingGame.java
+++ JavaTutorial/src/NumberGuessingGame.java
@@ -6,20 +6,25 @@ public class NumberGuessingGame {
 	public static void main(String[] args) {
 		 // Given K trials
         int K = 5; 
-        int i, guess;
+        int i = 0, guess;
         Random rand = new Random();
+        
         Scanner input = new Scanner(System.in);
         int number = rand.nextInt(10);
+        System.out.println(number);
         boolean gameStatus = false;
+        int[] guessedNumbers = new int[5];
         System.out.println("Welcome to Guess number Game");
         System.out.println("Guess the number 0 - 99");
         while(K > 0) {
         	System.out.println("Guess the number:");
         	guess = input.nextInt();
+        	
         	if((guess < 0) || (guess > 99)) {
         		System.out.println("Please enter the number 0 - 99");
         		continue;
         	}
+        	guessedNumbers[i++] = guess;
         	if(guess == number) {
         		gameStatus = true;
         		break;
@@ -33,10 +38,13 @@ public class NumberGuessingGame {
         	System.out.println("Remaining trial: " + K);
 		}else {
 			System.out.println("You have exhausted K trials.");
-	        System.out.println("The number was " + number);
+	        System.out.println("The number was: " + number);
 		}
-        
-        
+        System.out.print("Guessed numbers: ");
+        for(int value: guessedNumbers) {
+        	if (value != 0)
+        		System.out.print(value + " ");
+        }
         
         
         
