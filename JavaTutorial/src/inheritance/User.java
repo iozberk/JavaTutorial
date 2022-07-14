af1df251435baad7da2e8f97fa0c398772daec10
iozberk <iozberks@gmail.com>
2022-07-14 20:09:19 +0300

    OOP 009 - Inheritance 002

--- JavaTutorial/src/inheritance/User.java
+++ JavaTutorial/src/inheritance/User.java
@@ -5,6 +5,16 @@ public class User {
 	private int password;
 	private boolean loginStatus;
 	
+	
+	
+	
+	public User(int userId, int password, boolean loginStatus) {
+		super();
+		this.userId = userId;
+		this.password = password;
+		this.loginStatus = loginStatus;
+	}
+
 	public void verifyLogin() {
 		System.out.println("Login Status");
 	}
