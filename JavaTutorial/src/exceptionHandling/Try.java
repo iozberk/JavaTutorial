41a59e7457008e419b0c170ea91ebc336f7dd265
iozberk <iozberks@gmail.com>
2022-07-26 20:38:51 +0300

    Exception Handling 001 - Try-Catch-Finally 002

--- JavaTutorial/src/exceptionHandling/Try.java
+++ JavaTutorial/src/exceptionHandling/Try.java
@@ -1,5 +1,9 @@
 package exceptionHandling;
 
 public class Try {
-    
+    public static void main(String[] args) {
+        //System.out.println("Test");
+        int a = 2/0;
+        System.out.println(a);
+    }
 }
