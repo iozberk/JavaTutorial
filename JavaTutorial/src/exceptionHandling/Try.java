afff2aa364af3b15111ae33acd48e9f5c8b41f8e
iozberk <iozberks@gmail.com>
2022-07-26 20:54:49 +0300

    Exception Handling 001 - Try-Catch-Finally 005

--- JavaTutorial/src/exceptionHandling/Try.java
+++ JavaTutorial/src/exceptionHandling/Try.java
@@ -8,9 +8,13 @@ public class Try {
             int a = 2/0; // Error line
             System.out.println(a);
             System.out.println("The try block will not run when an error occurs. Note: lines after error");
-        } catch (ArithmeticException e) {
+        //} catch (ArithmeticException e) {
+        } catch (Exception e) {
            // System.out.println("Error");
-           System.out.println(e.getMessage());
+           //System.out.println(e.getMessage());
+           System.out.println(e.toString()); 
+        }finally{
+            System.out.println("Finally worked");
         }
 
 
