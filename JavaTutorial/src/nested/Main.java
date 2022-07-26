7d498f51a47ddebb2c7883bc2ecdcd0b2bbbbf3c
iozberk <iozberks@gmail.com>
2022-07-26 21:16:30 +0300

    OOP 014 - Nested Class 001

---
+++ JavaTutorial/src/nested/Main.java
@@ -0,0 +1,15 @@
+package nested;
+
+public class Main {
+    public static void main(String[] args) {
+        // Nested class
+        // Inner class
+        //     static
+        //     non-static
+        // Local class 
+        // Anonymous class
+
+
+        
+    }
+}
