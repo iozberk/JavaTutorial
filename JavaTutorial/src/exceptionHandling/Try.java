1d2891c5fb8b9b02a19ce1cce75b9cb38893aec8
iozberk <iozberks@gmail.com>
2022-07-26 20:41:51 +0300

    Exception Handling 001 - Try-Catch-Finally 003

--- JavaTutorial/src/exceptionHandling/Try.java
+++ JavaTutorial/src/exceptionHandling/Try.java
@@ -2,8 +2,18 @@ package exceptionHandling;
 
 public class Try {
     public static void main(String[] args) {
-        //System.out.println("Test");
-        int a = 2/0;
-        System.out.println(a);
+        System.out.println("Welcome to page");
+        try {
+            int a = 2/0;
+            System.out.println(a);
+        } catch (ArithmeticException e) {
+           // System.out.println("Error");
+           System.out.println(e.getMessage());
+        }
+
+
+        System.out.println("Have a nice day");
     }
+
+    
 }
