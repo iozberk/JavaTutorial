d54135baf2670e9e6ac857a1c5971464162b06d7
iozberk <iozberks@gmail.com>
2022-07-28 13:10:42 +0300

    OOP 014 - Collection 001 - PriorityQueue

---
+++ JavaTutorial/src/collection/PriorityQueue .java
@@ -0,0 +1,17 @@
+package collection;
+
+public class PriorityQueue  {
+    public static void main(String[] args) {
+        PriorityQueue pQueue = new PriorityQueue();
+        pQueue.add(80);
+        pQueue.add(30);
+        pQueue.add(20);
+        pQueue.add(60);
+        //pQueue.
+
+        while(!pQueue.isEmpty()){
+            System.out.println(pQueue.poll());
+        }
+    }
+    
+}
